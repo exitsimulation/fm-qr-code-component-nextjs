27f7ed59dc5cd6820d6db1fdbd562c50c14842ee
exitsimulation <jablonski.aaron@gmail.com>
2022-12-06 00:51:18 +0100

    Compiler Fixes

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
     'react'
   ],
   rules: {
-    'prettier/prettier': 'off'
+    'prettier/prettier': 'off',
+    "@typescript-eslint/strict-boolean-expressions": "warn"
   }
 }
